75161dc3567157eae1ab6a0cb8432c3b9441210d
AlanRENAULT35 <r.alandu35@gmail.com>
2024-05-13 06:00:40 -0400

    TASK 1 TO 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
+const session = require('express-session')
 
 let users = [];
 
@@ -52,9 +53,45 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const review = req.query.reviews;
+    const username = req.session.username;
+    if(books[isbn].reviews[username])
+    {
+        books[isbn].reviews[username] = review;
+        res.send('review has been modified');
+    }
+    else {
+        books[isbn].reviews[username].push(review);
+        res.send('review has been added');
+    }
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.username;
+    //const user = req.body.username;
+    console.log(isbn);
+  
+    if (!username) {
+      return res.status(401).json({message: "Unauthorized"});
+    }
+  
+    if (!isValid(username)) {
+      return res.status(401).json({message: "Invalid username"});
+    }
+  
+    if (!books[isbn]) {
+      return res.status(400).json({message: "Invalid ISBN"});
+    }
+  
+    if (!books[isbn].reviews[username]) {
+      return res.status(400).json({message: "Review not found for the given ISBN and username"});
+    }
+  
+    delete books[isbn].reviews[username];
+    return res.status(200).json({message: "Review deleted successfully"});
+  });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
